37442db7ca6d99b559314083fd6059f025cb61be
Vedavathi-S <vedavathivedavathi18@gmail.com>
2025-09-06 08:17:55 +0530

    Initial commit on news app

---
+++ src/Components/Navbar.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class Navbar extends Component {
+  static propTypes = {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="/">NewsEverday</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarScroll">
+            <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style={{"--bs-scroll-height": "100px"}}>
+                <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li className="nav-item">
+                <a className="nav-link" href="/about">About</a>
+                </li>
+                
+                
+            </ul>
+            
+            </div>
+        </div>
+        </nav>
+      </div>
+    )
+  }
+}
+
+export default Navbar
