06af2549c80368ed74586cdc3748b87e7fa583da
Vedavathi-S <vedavathivedavathi18@gmail.com>
2025-10-16 17:13:00 +0530

    Deployed on git and reinitialized

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,7 +9,7 @@ export class Navbar extends Component {
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-            <a className="navbar-brand" href="/">NewsEverday</a>
+            <Link className="navbar-brand" to="/">NewsEverday</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
